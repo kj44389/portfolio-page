a50f1a42d79d2f48855a0b975383ca4842dda665
Jarosław Kudzia <jaroslawkudzia73@gmail.com>
2021-09-29 19:13:10 +0200

    menu bar animation

---
+++ vendor/js/eventListenery.js
@@ -0,0 +1,28 @@
+(() => {
+
+    // hide menu on X or clicling out of menu
+    window.addEventListener("click", (event) => {
+        let btn = document.querySelector('#menu-btn');
+        let label = document.querySelector('label[for="menu-btn"]');
+        let navlist = document.querySelector('.nav-list');
+
+        if (event.target === document.querySelector(".fa-times")) {
+            btn.classList.remove('checked');
+
+        }
+        else if (event.target !== btn && event.target !== label && event.target !== navlist) {
+            btn.classList.remove('checked');
+
+        }
+           
+       
+   }, true)
+    
+    // show menu
+    document.querySelector('label[for="menu-btn"]').addEventListener('click', (event) => {
+        if (event.target !== document.querySelector(".fa-times")) {
+            document.querySelector('#menu-btn').classList.add('checked')
+        }
+    })
+
+ })();
