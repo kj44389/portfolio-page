011e00c30e1c048e743ee77c146dbb5c737ae7f9
kj44389 <47088522+kj44389@users.noreply.github.com>
2021-11-04 17:39:18 +0100

    Added Scroll Reveal

--- assets/js/revealOnScroll.js
+++ assets/js/revealOnScroll.js
@@ -1,102 +1,82 @@
-(() => {
-  document.addEventListener("DOMContentLoaded", () => {
-    // SCROLL REVEAL
-    const sr = ScrollReveal();
+class scrollReveal {
+    constructor() {}
+    reveal({
+        element: element,
+        delay: delay = 300,
+        duration: duration = 300,
+        easing: easing = ' ease-in-out',
+        distance: distance = 200,
+        origin: origin = 'left',
+        interval: interval = 0,
+    }) {
+        const el = document.querySelectorAll(element);
+        let i = 0;
+        el.forEach((tmp) => {
+            tmp.dataset.transform = tmp.style.transform;
+            let observer = new IntersectionObserver(this.execute);
+            this.prepare(tmp, delay, duration, easing, distance, origin, interval, i++);
+            observer.observe(tmp);
+        });
+    }
 
-    //ABOUT ME
-    sr.reveal(".about-me-headline", {
-      delay: 400,
-      duration: 400,
-      easing: "ease-in-out",
-      distance: `100px`,
-      origin: "left",
-    });
-    sr.reveal(".about-me-text", {
-      delay: 500,
-      duration: 400,
-      easing: "ease-in-out",
-      distance: `100px`,
-      origin: "left",
-    });
-    sr.reveal(".about-me-image", {
-      delay: 300,
-      duration: 400,
-      easing: "ease-in-out",
-      distance: `100px`,
-      origin: "left",
-    });
+    execute(entries, observer) {
+        entries.forEach((entry) => {
+            if (entry.intersectionRatio > 0) {
+                entry.target.style.transform = entry.target.dataset.transform;
+                entry.target.style.webkitTransform = entry.target.dataset.transform;
+                entry.target.style.opacity = 1;
+                entry.target.style.visibility = 'visible';
+                delete entry.target.dataset.transform;
+            }
+        });
+    }
 
-    //SKILLS
-    sr.reveal(".skills-header", {
-      delay: 300,
-      duration: 400,
-      easing: "ease-in-out",
-      distance: `100px`,
-      origin: "top",
-    });
-    sr.reveal(".skills-level-header", {
-      delay: 400,
-      duration: 400,
-      easing: "ease-in-out",
-      distance: `100px`,
-      origin: "top",
-    });
-    sr.reveal(".skill", {
-      delay: 500,
-      duration: 400,
-      easing: "ease-in-out",
-      distance: `100px`,
-      origin: "left",
-      interval: 16,
-    });
+    prepare(el, delay, duration, easing, distance, origin, interval, i) {
+        delay += (interval*i);
+        el.style.transition = `all ${duration}ms ${easing} ${delay}ms`;
+        el.style.webkitTransition = `all ${duration}ms ${easing} ${delay}ms`;
+        el.style.opacity = 0;
+        el.style.visibility = 'hidden';
 
-    //PROJECTS
-    sr.reveal(".projects-header", {
-      delay: 300,
-      duration: 400,
-      easing: "ease-in-out",
-      distance: `100px`,
-      origin: "top",
-    });
-    sr.reveal(".project", {
-      delay: 500,
-      duration: 400,
-      easing: "ease-in-out",
-      distance: `100px`,
-      origin: "left",
-      interval: 40,
-    });
-
-    //CONTACT
-    sr.reveal(".contact-header", {
-      delay: 300,
-      duration: 400,
-      easing: "ease-in-out",
-      distance: `100px`,
-      origin: "top",
-    });
-    sr.reveal(".form-inputs", {
-      delay: 500,
-      duration: 400,
-      easing: "ease-in-out",
-      distance: `100px`,
-      origin: "left",
-      interval: 40,
-    });
-    sr.reveal(".input-group label", {
-      delay: 500,
-      duration: 400,
-      easing: "ease-in-out",
-      distance: `100px`,
-      origin: "left",
-      interval: 40,
-    });
-    sr.reveal("#form-submit", {
-      delay: 700,
-      duration: 400,
-      easing: "ease-in-out",
-      distance: `100px`,
-      origin: "left",
-    });
-  });
-})();
+        switch (origin) {
+            case 'left':
+                el.style.transform = `matrix3d(1,0,0.00,0,0.00,1,0.00,0,0,0,1,0,-${distance},0,0,1) translateX(-50%)`;
+                el.style.webkitTransform = `matrix3d(1,0,0.00,0,0.00,1,0.00,0,0,0,1,0,-${distance},0,0,1) translateX(-50%)`;
+                break;
+            case 'right':
+                el.style.transform = `matrix3d(1,0,0.00,0,0.00,1,0.00,0,0,0,1,0,${distance},0,0,1) translateX(50%)`;
+                el.style.webkitTransform = `matrix3d(1,0,0.00,0,0.00,1,0.00,0,0,0,1,0,${distance},0,0,1) translateX(50%)`;
+                break;
+            case 'top':
+                el.style.transform = `matrix3d(1,0,0.00,0,0.00,1,0.00,0,0,0,1,0,0,${distance},0,1) translateY(50%)`;
+                el.style.webkitTransform = `matrix3d(1,0,0.00,0,0.00,1,0.00,0,0,0,1,0,0,${distance},0,1) translateY(50%)`;
+                break;
+            case 'bottom':
+                el.style.transform = `matrix3d(1,0,0.00,0,0.00,1,0.00,0,0,0,1,0,0,-${distance},0,1) translateY(-50%)`;
+                el.style.webkitTransform = `matrix3d(1,0,0.00,0,0.00,1,0.00,0,0,0,1,0,0,-${distance},0,1) translateY(-50%)`;
+                break;
+        }
+    }
+}
+;(() => {
+         document.addEventListener('DOMContentLoaded', () => {
+                  // SCROLL REVEAL
+                  window.sr = new scrollReveal()
+                  //about me
+                  sr.reveal({element: '.about-me-headline', delay : 300, duration : 400, distance : 50, origin: 'top'})
+                  sr.reveal({element: '.about-me-text', delay : 400, duration : 400, distance : 50, origin: 'left'})
+                  sr.reveal({element: '.about-me-image', delay : 300, duration : 400, distance : 50, origin: 'left'})
+                  //skills
+                  sr.reveal({element: '.skills-header', delay : 200, duration : 400, distance : 50, origin: 'top'})
+                  sr.reveal({element: '.skills-level-header', delay : 300, duration : 400, distance : 50, origin: 'top'})
+                  sr.reveal({element: '.skill', delay : 400, duration : 400, distance : 50, origin: 'left', interval: 60})
+                  //projects
+                  sr.reveal({element: '.projects-header', delay : 200, duration : 400, distance : 50, origin: 'top'})
+                  sr.reveal({element: '.project', delay : 400, duration : 400, distance : 50, origin: 'left', interval: 60})
+                  //contact
+                  sr.reveal({element: '.contact-header', delay : 200, duration : 400, distance : 50, origin: 'top'})
+                  sr.reveal({element: '.form-inputs', delay : 400, duration : 400, distance : 50, origin: 'left'})
+                  sr.reveal({element: '.input-group > label', delay : 400, duration : 400, distance : 50, origin: 'left'})
+                  sr.reveal({element: '#form-submit', delay : 400, duration : 400, distance : 50, origin: 'left'})
+         })
+})()
